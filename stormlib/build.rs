135bf2ea6dd7d59ad7f38bbf89261e3cec5b9cab
arenacraftwow <arenacraftwow@gmail.com>
2021-02-28 01:10:53 +0100

    Dep/stormlib: fix windows build

--- stormlib/build.rs
+++ stormlib/build.rs
@@ -2,9 +2,13 @@ use cmake;
 use cmake::Config;
 
 fn main() {
-    println!("cargo:rustc-link-lib=bz2");
-    println!("cargo:rustc-link-lib=z");
-    println!("cargo:rustc-link-lib=stdc++");
+    if !cfg!(windows) {
+        // stormlib has those builtin for windows
+        // but we need to link them in on other platforms
+        println!("cargo:rustc-link-lib=bz2");
+        println!("cargo:rustc-link-lib=z");
+        println!("cargo:rustc-link-lib=stdc++");
+    }
     let dst = Config::new("StormLib")
         .build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
